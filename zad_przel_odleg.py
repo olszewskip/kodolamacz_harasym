3228b81fcd8a81c83220941b46b6306d99a5596c
ozikson <olspaw@gmail.com>
2018-06-09 17:02:37 +0200

    astrotech_book zloz_typy_danych zadanie_na_koncu_2

---
+++ zad_przel_odleg.py
@@ -0,0 +1,13 @@
+def przelicz_metry(metry: int):
+    kilometry = 1_000 * metry
+    mile = float(1608 * metry)
+    mile_morskie = float(1852 * metry)
+
+    return {
+        'kilometers': kilometry,
+        'miles': mile,
+        'nautical miles': mile_morskie,
+        'all': [kilometry, mile, mile_morskie]
+            }
+
+print(przelicz_metry(5456))
